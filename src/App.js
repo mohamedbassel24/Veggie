82e4ca234cb13c11c48a60c060d499af03b5c88d
Khalifa1997 <ahmed70000@gmail.com>
2019-06-13 15:19:31 +0200

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/navBar/navBar";
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
